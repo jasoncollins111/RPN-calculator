76c2a65bf7f8ab3d89ddd3c182e95ef9b9bbf48a
Jason Collins <jasoncollins@Jasons-MacBook-Air.local>
2022-11-13 17:48:43 -0700

    remove uneeded code

--- commands/calculator.js
+++ commands/calculator.js
@@ -4,11 +4,9 @@ const clear = require('./clear');
 const handleCalculation = require('../services/calculator')
 
 async function calculator(tokens){
-      process.stdout.write(`tokens = ${tokens} \n`)
       let calculation = conf.get('calculation') || [];
       const newToken = tokens.trim().split(' ');
       calculation = [...calculation, ...newToken];
-      conf.set('calculation', calculation)
       handleCalculation(calculation) 
       conf.set('calculation', calculation);
 }
